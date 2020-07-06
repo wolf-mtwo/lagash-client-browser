a9381d2b3a854ed3dd9d73a2beef26c39e8398e7
wolf-mtwo <wolf.mtwo@outlook.com>
2020-07-06 01:38:30 -0400

    Header implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -84,6 +84,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NgbAlertModule,
     HttpClientModule,
     FormsModule,
+    MatIconModule,
 
     BrowserModule,
     AppRoutingModule,
