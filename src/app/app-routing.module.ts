340f4958567f523b481c5382bd5aca7aa015a585
wolf-mtwo <wolf.mtwo@outlook.com>
2020-07-05 20:26:37 -0400

    tabs were update to 2 spaces

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-// import { HeroesComponent }      from './heroes/heroes.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserComponent } from './browser/component';
 
